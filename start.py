ffb077f22a86870c45d6a6da3b43a1da34217a03
William Friefeldt <william@gmail.com>
2020-09-01 23:57:41 +0200

    MAJOR progress (get data from FB and save txt file)

--- start.py
+++ start.py
@@ -1,5 +1,8 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, send_file
 import pyrebase
+from collections import OrderedDict
+import os
+import keyboard as key
 
 config = {
     "apiKey": "AIzaSyC3Jx94GLlQlmMd36cFYonw2MrfTXf4YPE",
@@ -14,16 +17,61 @@ config = {
 
 app = Flask(__name__)
 
+podcasts = []
+pod = []
+episode = []
+
+def getPodcasts():
+    if len(podcasts) == 0:
+        firebase = pyrebase.initialize_app(config)
+        db = firebase.database()
+        data = db.child().get()
+        values = data.val()
+        for key in values:  
+            podcasts.append( { 'title': key, 'episodes':values[key] } )
+    return podcasts  
+
 @app.route('/')
 def index():
-	return render_template('main.html')
+    podcasts = getPodcasts()
+    episodes = None
+    return render_template('main.html', data = { 'podcasts': podcasts, 'episodes': None})
 
 @app.route('/handle_podcast', methods=['POST'])
 def handle_podcast():
-	firebase = pyrebase.initialize_app(config)
-	db = firebase.database()
-	user = db.child("alexochsigge").get()
-	ep = user.val()['ep1']
-	#podcast = request.form['podcast']
+    podcasts = getPodcasts()
+    podcasts.sort(key=lambda x: x['title'])
+    pod = request.form['pod']
+    i = 0
+    while i < len(podcasts):
+        if(pod == podcasts[i]['title']):
+            podcast = podcasts[i]
+            del podcasts[i]
+            podcasts.insert(0, podcast)
+        i += 1
+    episodes = podcast['episodes']
+    return render_template('main.html', data = { 'podcasts': podcasts, 'episodes': episodes})
 
-	return render_template('main.html', podcast = ep['name'])
+@app.route('/handle_episode', methods=['POST'])
+def handle_episode():
+    podcasts = getPodcasts()
+    episodes = podcasts[0]['episodes']
+    episode = request.form['episode']
+    i = 0
+    for ep in episodes:
+        if episodes[ep]['name'].strip() == episode:
+            chosenEp = episodes[ep]
+            firstEp = episodes['ep1']
+            del episodes[ep]
+            episodes['ep1'] = chosenEp
+            episodes[ep] = firstEp
+            break
+    return render_template('main.html', data = { 'podcasts': podcasts, 'episodes': episodes, 'episode': episode })
+
+@app.route('/to_recording', methods=['POST'])
+def to_recording():
+    return render_template('recording.html')
+
+@app.route('/download.txt')
+def download():
+    return send_file('test_res.txt', attachment_filename='')
